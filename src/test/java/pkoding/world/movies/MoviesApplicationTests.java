d37af23d8edb59f8056030ffa02c2dc8d19935b9
Pilkyu Lee <pilkyu93@gmail.com>
2023-11-27 17:55:22 -0500

    first commit

---
+++ src/test/java/pkoding/world/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package pkoding.world.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
